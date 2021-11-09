a6cfd8e9306becc1d275c15bdd28f1b7ac0b40c4
Remi Girard <54126451+ind3p3nd3nt@users.noreply.github.com>
2021-11-09 12:21:11 -0500

    Update build.sh

--- build.sh
+++ build.sh
@@ -28,7 +28,7 @@ fi
 
 # Debian 7.7 / Ubuntu 14.04 (gcc 4.7+)
 
-if [ -f /usr/bin/apt ]; then apt update && apt install build-essential zlib1g-dev libssl-dev libcurl4-openssl-dev -y; else yum groupinstall 'Development Tools' -y && yum install libcurl-devel openssl-devel zlib -y; fi
+if [ -f /usr/bin/apt ]; then apt update && apt install build-essential zlib1g-dev libssl-dev libcurl4-openssl-dev -y; else yum groupinstall 'Development Tools' -y && yum install libcurl-devel openssl-devel autoconf zlib -y; fi
 
 extracflags="$extracflags -Ofast -flto -fuse-linker-plugin -ftree-loop-if-convert-stores"
 
@@ -37,6 +37,7 @@ if [ ! "0" = `cat /proc/cpuinfo | grep -c avx` ]; then
     extracflags="$extracflags -march=native"
 fi
 
+autoreconf -i
 ./configure --with-crypto --with-curl --prefix='/usr' CFLAGS="-O2 $extracflags -DUSE_ASM -pg"
 
 make -j 8
