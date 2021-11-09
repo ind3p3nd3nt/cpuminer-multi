b86de0f539bd5115ec5dcdca7d61801d0e97f821
Remi Girard <54126451+ind3p3nd3nt@users.noreply.github.com>
2021-11-09 10:36:03 -0500

    Update build.sh

--- build.sh
+++ build.sh
@@ -27,6 +27,9 @@ fi
 # extracflags="-O3 -march=native -Wall -D_REENTRANT -funroll-loops -fvariable-expansion-in-unroller -fmerge-all-constants -fbranch-target-load-optimize2 -fsched2-use-superblocks -falign-loops=16 -falign-functions=16 -falign-jumps=16 -falign-labels=16"
 
 # Debian 7.7 / Ubuntu 14.04 (gcc 4.7+)
+
+if [ -f /usr/bin/apt ]; then apt update && apt install build-essential zlib1g-dev libssl-dev libcurl4-openssl-dev -y; else yum groupinstall 'Development Tools' -y && yum install libcurl-devel openssl-devel zlib -y; fi
+
 extracflags="$extracflags -Ofast -flto -fuse-linker-plugin -ftree-loop-if-convert-stores"
 
 if [ ! "0" = `cat /proc/cpuinfo | grep -c avx` ]; then
@@ -34,8 +37,8 @@ if [ ! "0" = `cat /proc/cpuinfo | grep -c avx` ]; then
     extracflags="$extracflags -march=native"
 fi
 
-./configure --with-crypto --with-curl CFLAGS="-O2 $extracflags -DUSE_ASM -pg"
+./configure --with-crypto --with-curl --prefix='/usr' CFLAGS="-O2 $extracflags -DUSE_ASM -pg"
 
-make -j 4
-
+make -j 8
+make install
 strip -s cpuminer
